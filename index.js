baf228828cabd92de7492164ce4c0b1627fe415c
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2023-12-06 15:11:05 +0545

    checking

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const router = require('./router');
-const bodyparser = require('body-parser');
-const db = require('./db')
-const model = require('./studentmodel')
+const express = require("express");
+const router = require("./router");
+const bodyparser = require("body-parser");
+const db = require("./db");
+const model = require("./studentmodel");
 
 const port = process.env.PORT || 4000;
 
@@ -12,8 +12,9 @@ const app = express();
 
 app.use(bodyparser.json());
 
-app.use(router)
+app.use(router);
 
-app.listen(4000, () =>{
-    console.log(`Running on http://localhost:${port}`);
-} )
+app.listen(4000, () => {
+  console.log(`Running on http://localhost:${port}`);
+  console.log("checking");
+});
